6bed0772b95c1731c01d8f7d02c688527f4d4c1e
Laura Fretter <fretterlaura@gmail.com>
2019-03-01 10:30:40 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -10,9 +10,8 @@ function forLoop(array){
   return array
 }
 function whileLoop(n) {
-  while(n => 0){
-    console.log(n);
-    n--
+  while(n >= 0){
+    console.log(--n);
   }
   console.log("done")
 }
